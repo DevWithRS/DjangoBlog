dc2f38e6ab510d648cf34efcddaac4722ceb6dad
Rezwan <rezwanmshaijad122007@outlook.com>
2025-08-14 14:40:10 +0100

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+print("blog.urls loaded")
+
+
+urlpatterns = [
+    path("", views.home, name='home'),
+    path("post/<int:pk>", views.post_details, name="post-detail"),
+    path("post/new/", views.PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
+]
