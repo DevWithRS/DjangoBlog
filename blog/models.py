647c13b8ff38fe481bb036a941a6c13d4c7a09d6
Rezwan <rezwanmshaijad122007@outlook.com>
2025-08-22 19:28:07 +0100

    Updated the code so that users cannot edit/delete posts published by other users.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 
@@ -8,6 +10,11 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from .models import Post
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 # Create your views here.
 
@@ -24,25 +26,37 @@ def post_details(request, pk):
 # New Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "blog/post_form.html"
     fields = ["title", "content"]
     success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 # Update
 
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = "blog/post_form.html"
     fields = ["title", "content"]
     success_url = reverse_lazy("home")
 
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
 # Delete
 
 
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "blog/post_delete.html"
     success_url = reverse_lazy("home")
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
